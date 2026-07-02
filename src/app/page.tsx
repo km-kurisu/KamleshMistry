54b512f67e503f6b05286b0247571fdec8d688d4
km-kurisu <kamleshkmistry33@gmail.com>
2026-07-02 17:19:17 +0530

    added vercel analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import About from "@/components/About";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function Home() {
   return (
